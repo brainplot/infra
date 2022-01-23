2165dca1010fb83313e2a8e93b29d7cfba6025bc
Gianluca Recchia <gianluca.recchia@protonmail.com>
2022-01-23 12:13:05 +0100

    Generate Cloudflare API token for Certbot

--- dns.tf
+++ dns.tf
@@ -31,3 +31,27 @@ resource "cloudflare_record" "www" {
   value   = cloudflare_record.root_ipv4.hostname
   type    = "CNAME"
 }
+
+data "cloudflare_api_token_permission_groups" "all" {}
+
+resource "cloudflare_api_token" "certbot" {
+  name = "certbot"
+
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.permissions["DNS Write"],
+    ]
+    resources = {
+      "com.cloudflare.api.account.zone.${cloudflare_zone.zone.id}" = "*",
+    }
+  }
+
+  condition {
+    request_ip {
+      in = [
+        "${digitalocean_droplet.web.ipv4_address}/32",
+        "${digitalocean_droplet.web.ipv6_address}/128",
+      ]
+    }
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "web_ipv4_address" {
 output "web_ipv6_address" {
   value = digitalocean_droplet.web.ipv6_address
 }
+
+output "certbot_cloudflare_api_token" {
+  value     = cloudflare_api_token.certbot.value
+  sensitive = true
+}
